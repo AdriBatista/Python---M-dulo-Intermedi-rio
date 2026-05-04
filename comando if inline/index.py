711442e65a3dd3aaff792204cda91a99dd788901
adriano da silva batista <adrianobatista@outlook.com.br>
2026-05-04 20:55:31 -0300

    Create index.py

---
+++ comando if inline/index.py
@@ -0,0 +1,17 @@
+#Usando if e else na mesma linha de comando sem identação
+
+X = 10
+Y = 9
+
+if X >= Y:
+    print(X)
+else:
+    print(Y)    
+
+print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")    
+
+print(X) if X >= Y else print(Y)
+
+print("=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=xx=x=")   
+
+print(f'O valor X é {X}') if X <= Y else print(f'O valor de Y é {Y}')
