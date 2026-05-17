b35697acb5092c352d65addb9f7343eec341922d
adriano da silva batista <adrianobatista@outlook.com.br>
2026-05-17 16:06:20 -0300

    Update index.py

--- conjuntos/index.py
+++ conjuntos/index.py
@@ -1,2 +1,14 @@
 
-c1 = {16, 8, 21, 30}
+#vpara os conjuntos usa-se chaves {}
+c1 = {16, 8, 21, 30, 41, 28}
+
+
+# o tipo é uma classe 'set' que contem os metodos que permite trabalhas com CONJUNTOS
+a = type(c1)
+print(a)
+
+print('=============++++++++++++++++++')
+
+#vamor ver o tamanho de elementos que contem essa chave
+b = len(c1)
+print(b)
